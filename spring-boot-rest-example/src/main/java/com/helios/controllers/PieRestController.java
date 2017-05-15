f936e337f62c4656aa975a4c809d3f8fd65b0861
bdansoko <bdansoko@LFR011926.corp.capgemini.com>
2017-05-15 18:15:13 +0200

    second commit

--- spring-boot-rest-example/src/main/java/com/helios/controllers/PieRestController.java
+++ spring-boot-rest-example/src/main/java/com/helios/controllers/PieRestController.java
@@ -1,15 +1,7 @@
 package com.helios.controllers;
 
-import com.helios.models.Pie;
-import com.helios.repositories.PieRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Collection;
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by kaspernissen on 11/05/2017.
@@ -18,26 +10,39 @@ import java.util.List;
 @RequestMapping("/pies")
 public class PieRestController {
 
-        @Autowired
-        private PieRepository repository;
+//        @Autowired
+//        private PieRepository repository;
+//
+//        @RequestMapping(method = RequestMethod.GET)
+//        public ResponseEntity<Collection<Pie>> getAllPies(){
+//            return new ResponseEntity<>((Collection<Pie>) repository.findAll(), HttpStatus.OK);
+//        }
+//
+//        @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+//        public ResponseEntity<Pie> getPieWithId(@PathVariable Long id) {
+//            return new ResponseEntity<>(repository.findOne(id),HttpStatus.OK);
+//        }
+//
+//        @RequestMapping(method = RequestMethod.GET, params = {"name"})
+//        public ResponseEntity<Collection<Pie>> findPieWithName(@RequestParam(value="name") String name) {
+//            return new ResponseEntity<>(repository.findByName(name), HttpStatus.OK);
+//        }
+//
+//        @RequestMapping(method = RequestMethod.POST)
+//        public ResponseEntity<?> addPie(@RequestBody Pie input) {
+//            return new ResponseEntity<>(repository.save(input), HttpStatus.CREATED);
+//        }
+        
 
-        @RequestMapping(method = RequestMethod.GET)
-        public ResponseEntity<Collection<Pie>> getAllPies(){
-            return new ResponseEntity<>((Collection<Pie>) repository.findAll(), HttpStatus.OK);
-        }
-
-        @RequestMapping(method = RequestMethod.GET, value = "/{id}")
-        public ResponseEntity<Pie> getPieWithId(@PathVariable Long id) {
-            return new ResponseEntity<>(repository.findOne(id),HttpStatus.OK);
-        }
-
-        @RequestMapping(method = RequestMethod.GET, params = {"name"})
-        public ResponseEntity<Collection<Pie>> findPieWithName(@RequestParam(value="name") String name) {
-            return new ResponseEntity<>(repository.findByName(name), HttpStatus.OK);
-        }
-
-        @RequestMapping(method = RequestMethod.POST)
-        public ResponseEntity<?> addPie(@RequestBody Pie input) {
-            return new ResponseEntity<>(repository.save(input), HttpStatus.CREATED);
-        }
+        
+        
+        
+        /**
+         * Service qui permet d'obtenir un interlocuteur à partir de son id
+         */
+        
+//        @RequestMapping(method = RequestMethod.GET, params = {"name"})
+//        public ResponseEntity<Collection<Interlocuteur>> getInterlocuteur(@RequestParam(value="id") String id) {
+//            return new ResponseEntity<>(repository.findByName(id), HttpStatus.OK);
+//        }
 }
