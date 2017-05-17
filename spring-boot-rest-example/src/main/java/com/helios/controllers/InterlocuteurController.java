a23c063de922e78a633d2d2e88351963b29f7a69
yaelayac <yaelayac@LFR016468.corp.capgemini.com>
2017-05-17 15:07:57 +0200

    commit 8

--- spring-boot-rest-example/src/main/java/com/helios/controllers/InterlocuteurController.java
+++ spring-boot-rest-example/src/main/java/com/helios/controllers/InterlocuteurController.java
@@ -1,17 +1,31 @@
 package com.helios.controllers;
 
+import javax.websocket.server.PathParam;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.helios.models.Interlocuteur;
 
-@RestController
-@RequestMapping("/interlocuteurs")
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
+@RequestMapping("/vue360/interlocuteur")
 public interface InterlocuteurController {
 
+
+    
+    
+    
+    /**
+     * Récupération des données concernant la vue 360° interlocuteur
+     * @param id
+     * @return
+     */
+    @ApiOperation(value="Récupération des données concernant la vue 360° interlocuteur", notes="Service NAMEK")
     @RequestMapping(method = RequestMethod.GET, params = {"id"}, value = "/{id}")
-    public ResponseEntity<Interlocuteur> getInterlocuteur(@RequestParam(value="id") String id);
+  	public ResponseEntity<Interlocuteur> getInterlocuteur(@ApiParam(value = "id intelocuteur") @PathParam("id intelocuteur") String id);
+  	
 }
--- spring-boot-rest-example/src/main/java/com/helios/controllers/InterlocuteurControllerImpl.java
+++ spring-boot-rest-example/src/main/java/com/helios/controllers/InterlocuteurControllerImpl.java
@@ -3,27 +3,33 @@ package com.helios.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.helios.models.Interlocuteur;
 import com.helios.repositories.InterlocuteurRepository;
 
-import io.swagger.annotations.ApiOperation;
-
 @RestController
-@RequestMapping("/vue360/interlocuteur")
-public class InterlocuteurControllerImpl {
+public class InterlocuteurControllerImpl implements InterlocuteurController {
 
 	
   @Autowired
   private InterlocuteurRepository repository;
-	
-  @ApiOperation(value="Chercher interlocuteur par id", notes="Permet de rechercher un interlocuteur à partir de son id")
-  @RequestMapping(method = RequestMethod.GET, params = {"id"}, value = "/{id}")
-	public ResponseEntity<Interlocuteur> getInterlocuteur(String id) {
-		return new ResponseEntity<>(repository.findOne(id), HttpStatus.OK);
-	}
+
+@Override
+public ResponseEntity<Interlocuteur> getInterlocuteur(String id) {
+	return new ResponseEntity<>(repository.findOne(id), HttpStatus.OK);
+}
+
+  
+//  /**
+//   * 
+//   * @param id
+//   * @return
+//   */
+//  @ApiOperation(value="Récupération des données concernant la vue 360° interlocuteur", notes="Service NAMEK")
+//  @RequestMapping(method = RequestMethod.GET, params = {"id"}, value = "/{id}")
+//	public ResponseEntity<Interlocuteur> getInterlocuteur(@ApiParam(value = "id intelocuteur") @PathParam("id intelocuteur") String id) {
+//		return new ResponseEntity<>(repository.findOne(id), HttpStatus.OK);
+//	}
 
 }
